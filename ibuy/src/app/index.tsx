ab651ea2498251720f30eabe2c2217c243bdf4bc
gustavoaesteveswehandle <gustavo.esteves@wehandle.com.br>
2025-07-10 23:24:02 -0300

    feat: pagina bases

---
+++ ibuy/src/app/index.tsx
@@ -0,0 +1,25 @@
+﻿import { View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router'; 
+ 
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href="/signup" style={styles.new}>
+        Criar conta
+      </Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  new: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
---
+++ ibuy/src/app/signup.tsx
@@ -0,0 +1,23 @@
+﻿import { View, Text, StyleSheet } from 'react-native';
+ 
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Criar novo login</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
