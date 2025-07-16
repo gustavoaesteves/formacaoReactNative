1742a38d79631ccc50f87bad3a6ae600e861eb10
gustavoaesteves <gustavo.esteves@wehandle.com.br>
2025-07-16 16:24:06 -0300

    fix: layout

--- ibuy/src/app/_layout.tsx
+++ ibuy/src/app/_layout.tsx
@@ -1,7 +1,42 @@
-﻿import { Stack } from "expo-router";
+﻿import { Drawer} from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
+    // return (
+    //     <Tabs 
+    //     screenOptions={{ 
+    //         headerShown: false, 
+    //         tabBarShowLabel: false,
+    //         tabBarLabelPosition: "beside-icon",
+    //         }}
+    //         >
+    //         <Tabs.Screen
+    //             name="index"
+    //             options={{ 
+    //                 tabBarLabel: "Home",
+    //                 // tabBarIcon: ({ color }) => (
+    //                 //     <Icon name="home" color={color} />
+    //                 // ),
+    //                 title: "Home" }}/>
+    //         <Tabs.Screen
+    //             name="signup"
+    //             options={{ title: "Login" }}
+    //         />    
+    //     </Tabs>
+    // )
+
     return (
-        <Stack screenOptions={{ headerShown: false }} />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={{ title: "Home" }}
+                />
+                <Drawer.Screen
+                    name="signup"f
+                    options={{ title: "Login" }}
+                />    
+            </Drawer>
+        </GestureHandlerRootView>
     )
 }
